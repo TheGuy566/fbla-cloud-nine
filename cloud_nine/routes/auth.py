1d7fe3d4cb34ff537b48b4fe877512f674e72de6
TheGuy566 <54692320+TheGuy566@users.noreply.github.com>
2019-12-07 21:25:48 -0500

    Add files via upload

--- cloud_nine/routes/auth.py
+++ cloud_nine/routes/auth.py
@@ -2,7 +2,7 @@ from cloud_nine.extensions import db
 from cloud_nine.models import User, Flights
 from flask_login import login_user, logout_user
 from werkzeug.security import check_password_hash
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 auth = Blueprint("auth", __name__)
 
@@ -12,7 +12,37 @@ def signup():
         name = request.form["name"]
         unhashed_password = request.form["password"]
 
-        user = User(name=name, unhashed_password=unhashed_password)
+        if len(name) == 0:
+            flash("Please enter a username.", "warning")
+            return render_template("signup.html")
+        
+        if len(unhashed_password) == 0:
+            flash("Please enter a password.", "warning")
+            return render_template("signup.html")
+
+        if len(name) <= 6:
+            flash("Username must be 7 characters or longer.", "warning")
+            return render_template("signup.html")
+        
+        if len(unhashed_password) <= 4:
+            flash("Password must be 5 characters or longer.", "warning")
+            return render_template("signup.html")
+
+        matched_users = User.query\
+            .filter_by(name=name)\
+            .all()
+        
+        if matched_users:
+            flash("Username is already in use.", "danger")
+            return render_template("signup.html")
+
+        user = User(
+            name=name,
+            unhashed_password=unhashed_password,
+            miles_flown=0,
+            flyer_miles=0,
+            cash_spent=0
+        )
         
         db.session.add(user)
         db.session.commit()
@@ -25,22 +55,27 @@ def login():
         name = request.form["name"]
         password = request.form["password"]
 
+        if len(name) == 0:
+            flash("Please enter a username.", "warning")
+            return render_template("login.html")
+        if len(password) == 0:
+            flash("Please enter a password.", "warning")
+            return render_template("login.html")
+
         user = User.query.filter_by(name=name).first()
-        error_msg = ""
 
         if not user or not check_password_hash(user.password, password):
-            error_msg = "Could not login. Please check and try again"
-        if not error_msg:
-            login_user(user)
-            return redirect(url_for("main.index"))
+            flash("Invalid Username or Password.", "danger")
+            return render_template("login.html")
+        
+        login_user(user)
+        flash("Login Successful!", "success")
+        return redirect(url_for("main.index"))
         
     return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
     logout_user()
+    flash("Logout Successful!", "success")
     return redirect(url_for("main.index"))
-
-@auth.route("/profile")
-def profile():
-    return render_template("profile.html")
--- cloud_nine/routes/main.py
+++ cloud_nine/routes/main.py
@@ -1,7 +1,45 @@
 from cloud_nine.extensions import db
 from cloud_nine.models import User, Flights
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, session, escape, flash
 from flask_login import current_user, login_required
+from math import sin, cos, sqrt, atan2, radians
+import datetime
+
+cities = ['Atlanta', 'Austin', 'Baton Rogue', 'Columbia', 'D.C.', 'Jackson', 'Las Vegas',
+'Little Rock','Long Beach', 'Miami', 'Montgomery', 'Nashville', 'New York', 'Olympia',
+'Orlando', 'Raligh', 'Sacremento', 'Salem', 'Sanfrancico', 'Tallahasse']
+
+positions = {'Atlanta':[33.7490, 84.3880], 'Austin':[30.2672, 97.7431], 'Baton Rogue':[30.4515, 91.1871], 'Columbia':[34.0007, 81.0348],
+'D.C.':[38.9072, 77.0369], 'Jackson':[32.2988, 90.1848], 'Las Vegas':[36.1699, 115.1398], 'Little Rock':[34.7465, 92.2896],
+'Long Beach':[33.7701, 118.1937], 'Miami':[25.7617, 80.1918], 'Montgomery':[32.3792, 86.3077], 'Nashville':[36.1627, 86.7816],
+'New York':[40.7128, 74.0060], 'Olympia':[47.0379, 122.9007], 'Orlando':[28.5383, 81.3792], 'Raligh':[35.7796, 78.6382],
+'Sacremento':[38.5816, 121.4944], 'Salem':[44.9429, 123.0351], 'Sanfrancico':[38.5816, 121.4944], 'Tallahasse':[30.4383, 84.2807]}
+
+brochures = {'Atlanta':"https://www.atlanta.net/explore/", 'Austin':"https://www.austintexas.org/plan-a-trip/",
+'Baton Rogue':"https://www.visitbatonrouge.com/things-to-do/",'Columbia':"https://www.experiencecolumbiasc.com/things-to-do/",
+'D.C.':"https://washington.org/things-do-washington-dc", 'Jackson':"https://www.visitjackson.com/must-see-and-do",
+'Las Vegas':"https://www.travelandleisure.com/travel-guide/las-vegas-nevada/things-to-do",
+'Little Rock':"https://www.travelandleisure.com/travel-guide/little-rock/things-to-do",
+'Long Beach':"https://www.visitlongbeach.com/", 'Miami':"https://www.travelandleisure.com/travel-guide/miami/things-to-do",
+'Montgomery':"https://www.lonelyplanet.com/usa/the-south/montgomery", 'Nashville':"https://www.travelandleisure.com/travel-guide/nashville-tennessee",
+'New York':"https://www.nycgo.com/", 'Olympia':"https://www.experienceolympia.com/explore/",
+'Orlando':"https://www.visitorlando.com/en/things-to-do", 'Raligh':"https://www.visitraleigh.com/things-to-do/",
+'Sacremento':"https://www.visitsacramento.com/visit/vistors-guide/", 'Salem':"https://www.travelsalem.com/things-to-do",
+'Sanfrancico':"https://www.travelandleisure.com/travel-guide/san-francisco/things-to-do", 'Tallahasse':"https://www.visitflorida.com/en-us/cities/tallahassee.html"}
+
+def calculate_distance(location_a, location_b):
+    R = 6373.0
+    lat1 = radians(location_a[0])
+    lon1 = radians(location_a[1])
+    lat2 = radians(location_b[0])
+    lon2 = radians(location_b[1])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = int(R * c)
+
+    return distance
 
 main = Blueprint("main", __name__)
 
@@ -13,25 +51,59 @@ def index():
 def about():
     return render_template("about.html")
 
-@main.route("/flights")
+@main.route("/flights", methods=['GET', 'POST'])
 def flights():
     if not current_user.is_authenticated:
         return redirect(url_for('auth.login'))
-
-    if request.method == "POST":
-        takeoff = request.form["name"]
-        landing = request.form["password"]
-
-        flight = Flights(takeoff=takeoff, landing=landing, flyer=current_user.id)
-        
-        db.session.add(flight)
-        db.session.commit()
     
-    booked_flights = Flights.query.filter_by(flyer=current_user.id).all()
-    context = {
-        'booked_flights' : booked_flights
-    }
+    if request.method == "POST":
+        takeoff = request.form["start_location"]
+        landing = request.form["end_location"]
+        date = request.form["date"]
 
+        flights = Flights(
+            takeoff=takeoff, 
+            landing=landing,
+            distance=calculate_distance(positions[takeoff], positions[landing]),
+            flyer_id=current_user.id,
+            date=date 
+        )
+
+        if takeoff == landing:
+            flash("Start location and end location should not be the same.", "warning")
+            return redirect(url_for('main.flights'))
+        
+        if date == "":
+            flash("Please specify a date for your flight.", "warning")
+            return redirect(url_for('main.flights'))
+
+        today = datetime.datetime.now()
+        formatted = today.strftime("%Y-%m-%d").split("-")
+        
+        if int(date.split("-")[0]) < int(formatted[0]) or \
+            int(date.split("-")[1]) < int(formatted[1]) or \
+            int(date.split("-")[2]) < int(formatted[2]):
+            flash("Please input a date that has not occurred.", "danger")
+            return redirect(url_for('main.flights'))
+
+        current_user.miles_flown += calculate_distance(positions[takeoff], positions[landing])
+        current_user.flyer_miles += calculate_distance(positions[takeoff], positions[landing])/2
+        current_user.cash_spent += round(50 + (calculate_distance(positions[takeoff], positions[landing])/1.609)*0.11, 2)
+        
+        db.session.add(flights)
+        db.session.commit()
+
+        flash("Flight booked!", "success")
+        return redirect(url_for('main.flights'))
+
+    flight_book = Flights.query\
+        .filter_by(flyer_id=current_user.id)\
+        .all()
+
+    context = {
+        'flight_book' : flight_book,
+        'locations': cities
+    }
     return render_template('flights.html', **context)
 
 @main.route("/programs")
@@ -41,6 +113,10 @@ def programs():
 @main.route("/work")
 def work():
     return render_template("work.html")
+
+@main.route("/flyers")
+def flyers():
+    return render_template("flyers.html")
 
 @main.route("/credits")
 def credits():
@@ -53,3 +129,62 @@ def privacy():
 @main.route("/terms")
 def terms():
     return render_template("terms.html")
+
+@main.route("/profile_stats")
+def profile_stats():
+    flight_book = Flights.query\
+        .filter_by(flyer_id=current_user.id)\
+        .all()
+
+    context = {
+        'name': current_user.name,
+        'miles': current_user.miles_flown,
+        'money': current_user.cash_spent,
+        'flight_book' : flight_book,
+        'flyer_mi': current_user.flyer_miles
+
+    }
+
+    return render_template("profile.html", **context)
+
+@main.route("/profile_notify")
+def profile_notify():
+    today = datetime.datetime.now()
+    formatted = today.strftime("%Y-%m-%d")
+
+    flight_book = Flights.query\
+        .filter_by(flyer_id=current_user.id)\
+        .filter_by(date=formatted)\
+        .all()
+
+    context = {
+        'name': current_user.name,
+        'flight_book' : flight_book,
+        'travel': brochures
+    }
+
+    return render_template("profile_notify.html", **context)
+
+@main.route("/profile_booked", methods=['GET', 'POST'])
+def profile_booked():
+    if request.method == 'POST':
+        delete_me = request.form["delete"]
+        delete_selected = Flights.query.filter_by(id=delete_me).first()
+
+        flash("Flight deleted!", "success")
+
+        db.session.delete(delete_selected)
+        db.session.commit()
+
+        return redirect(url_for('main.profile_booked'))
+    
+    flight_book = Flights.query\
+        .filter_by(flyer_id=current_user.id)\
+        .all()
+    
+    context = {
+        'name': current_user.name,
+        'flight_book' : flight_book,
+    }
+
+    return render_template("profile_booked.html", **context)
