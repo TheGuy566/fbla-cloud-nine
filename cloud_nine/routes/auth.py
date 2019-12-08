17778d5dc7e994abab3f0807aace805423487dbb
TheGuy566 <54692320+TheGuy566@users.noreply.github.com>
2019-12-07 21:54:38 -0500

    Update auth.py

--- cloud_nine/routes/auth.py
+++ cloud_nine/routes/auth.py
@@ -1,6 +1,6 @@
 from cloud_nine.extensions import db
 from cloud_nine.models import User, Flights
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 from werkzeug.security import check_password_hash
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 
